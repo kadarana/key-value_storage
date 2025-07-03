039ad4f2cde6730f248fe8a783a0b133ce649c3b
daria <m2403815@edu.misis.ru>
2025-07-04 01:06:59 +0300

    second commit

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"myproj/internal/pkg/storage"
+)
+
+func main() {
+	s, err := storage.NewStorage()
+	if err != nil {
+		panic(err)
+	}
+
+	s.Set("key1", "value1")
+	s.Set("key2", "1337")
+
+	fmt.Println(*s.Get("key1"))
+	fmt.Println(*s.Get("key2"))
+	fmt.Println(s.Get("key3"))
+
+	fmt.Println(s.GetType("key1"))
+	fmt.Println(s.GetType("key2"))
+	fmt.Println(s.GetType("key3"))
+
+}
