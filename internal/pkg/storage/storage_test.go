ff1fe3e77b2f4ed43e39ec79514e622e2eb286f7
daria <m2403815@edu.misis.ru>
2025-07-08 16:16:44 +0300

    2pt

---
+++ internal/pkg/storage/storage_test.go
@@ -0,0 +1,175 @@
+package storage
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+type TestCase struct {
+	name  string
+	key   string
+	value string
+}
+
+func TestSetGet(t *testing.T) {
+	cases := []TestCase{
+		{"1", "first", "world"},
+		{"2", "second", "2"},
+		{"3", "third", "3d"},
+	}
+
+	s, err := NewStorage()
+	if err != nil {
+		t.Errorf("no storage: %v", err)
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			s.Set(c.key, c.value)
+
+			sValue := s.Get(c.key)
+
+			if *sValue != c.value {
+				t.Errorf("values not equal")
+			}
+		})
+	}
+}
+
+func TestKind(t *testing.T) {
+	cases := []TestCase{
+		{"1", "first", "world"},
+		{"2", "second", "2"},
+		{"3", "third", "3d"},
+	}
+
+	s, err := NewStorage()
+	if err != nil {
+		t.Errorf("no storage: %v", err)
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			s.Set(c.key, c.value)
+			var kind string
+			_, err := strconv.Atoi(c.value)
+			if err != nil {
+				kind = "S"
+			} else {
+				kind = "D"
+			}
+			if s.inner[c.key].valueType != kind {
+				t.Errorf("kinds not equal")
+			}
+		})
+	}
+}
+
+var casebench = []TestCase{
+	{"Hello world", "Hello", "world"},
+	{"number1221", "number", "1221"},
+	{"number666", "number", "666"},
+}
+
+func BenchmarkGet(b *testing.B) {
+	s, _ := NewStorage()
+
+	for _, c := range casebench {
+		s.Set(c.key, c.value)
+	}
+
+	var results strings.Builder
+
+	for _, c := range casebench {
+		b.Run(c.name, func(bb *testing.B) {
+			bb.ResetTimer()
+
+			for i := 0; i < bb.N; i++ {
+				_ = s.Get(c.key)
+			}
+
+			results.WriteString(fmt.Sprintf(
+				"%s: %d ops, %v/op\n",
+				c.name,
+				bb.N,
+				bb.Elapsed()/time.Duration(bb.N),
+			))
+		})
+	}
+	SaveBenchMarkResults(results.String())
+
+}
+
+func BenchmarkSet(b *testing.B) {
+
+	var results strings.Builder
+
+	for _, c := range casebench {
+		b.Run(c.name, func(bb *testing.B) {
+			s, _ := NewStorage()
+			bb.ResetTimer()
+
+			for i := 0; i < bb.N; i++ {
+				s.Set(c.key, c.value)
+			}
+
+			results.WriteString(fmt.Sprintf(
+				"%s: %d ops, %v/op\n",
+				c.name,
+				bb.N,
+				bb.Elapsed()/time.Duration(bb.N),
+			))
+		})
+	}
+	SaveBenchMarkResults(results.String())
+
+}
+
+func BenchmarkSetGet(b *testing.B) {
+
+	var results strings.Builder
+
+	for _, c := range casebench {
+		b.Run(c.name, func(bb *testing.B) {
+			s, _ := NewStorage()
+			bb.ResetTimer()
+
+			for i := 0; i < bb.N; i++ {
+				s.Set(c.key, c.value)
+				_ = s.Get(c.key)
+			}
+
+			results.WriteString(fmt.Sprintf(
+				"%s: %d ops, %v/op\n",
+				c.name,
+				bb.N,
+				bb.Elapsed()/time.Duration(bb.N),
+			))
+
+		})
+	}
+	SaveBenchMarkResults(results.String())
+}
+
+func SaveBenchMarkResults(output string) {
+	if err := os.MkdirAll("benchmark_results", 0755); err != nil {
+		panic(err)
+	}
+
+	file, err := os.OpenFile(
+		"benchmark_results/results.txt",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0644,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+	if _, err := file.WriteString(output + "\n"); err != nil {
+		panic(err)
+	}
+}
