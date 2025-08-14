6c18f9f573970089fdad407832c3e56e09f7963f
daria <m2403815@edu.misis.ru>
2025-08-15 02:43:09 +0300

    delete dir

--- livecode/pt2/math/math.go
+++
@@ -1,5 +0,0 @@
-package math
-
-func Add(x, y int) (result int) {
-	return x + y
-}
--- livecode/pt5/internal/pkg/basic_http/types.go
+++
@@ -1,9 +0,0 @@
-package basic_http
-
-type X2Request struct {
-	Val int
-}
-
-type X2Response struct {
-	Val int
-}
